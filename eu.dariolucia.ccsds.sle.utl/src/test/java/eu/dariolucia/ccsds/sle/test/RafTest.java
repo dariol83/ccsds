85a1e6032b0c2230716d8068f951ee86c82b9d60
Dario <dario.lucia@gmail.com>
2019-09-29 18:30:01 +0200

    Extended SLE RAF test

--- eu.dariolucia.ccsds.sle.utl/src/test/java/eu/dariolucia/ccsds/sle/test/RafTest.java
+++ eu.dariolucia.ccsds.sle.utl/src/test/java/eu/dariolucia/ccsds/sle/test/RafTest.java
@@ -16,9 +16,8 @@
 
 package eu.dariolucia.ccsds.sle.test;
 
-import eu.dariolucia.ccsds.sle.generated.ccsds.sle.transfer.service.raf.outgoing.pdus.RafGetParameterReturn;
+import eu.dariolucia.ccsds.sle.generated.ccsds.sle.transfer.service.common.pdus.SleScheduleStatusReportReturn;
 import eu.dariolucia.ccsds.sle.generated.ccsds.sle.transfer.service.raf.outgoing.pdus.RafGetParameterReturnV1toV4;
-import eu.dariolucia.ccsds.sle.generated.ccsds.sle.transfer.service.raf.structures.RafGetParameterV1toV4;
 import eu.dariolucia.ccsds.sle.server.OperationRecorder;
 import eu.dariolucia.ccsds.sle.server.RafServiceInstanceProvider;
 import eu.dariolucia.ccsds.sle.utl.config.UtlConfigurationFile;
@@ -31,59 +30,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class RafTest {
 
     @Test
-    void testUserBindUnbind() throws IOException, InterruptedException {
-        // Full logs
-        Logger.getLogger("eu.dariolucia.ccsds").setLevel(Level.ALL);
-        for(Handler h : Logger.getLogger("").getHandlers()) {
-            h.setLevel(Level.ALL);
-        }
-        // Provider
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream("configuration_test_provider.xml");
-        UtlConfigurationFile providerFile = UtlConfigurationFile.load(in);
-        RafServiceInstanceConfiguration rafConfigP = (RafServiceInstanceConfiguration) providerFile.getServiceInstances().get(1); // RAF
-        RafServiceInstanceProvider rafProvider = new RafServiceInstanceProvider(providerFile.getPeerConfiguration(), rafConfigP);
-        rafProvider.waitForBind(true, null);
-
-        // User
-        in = this.getClass().getClassLoader().getResourceAsStream("configuration_test_user.xml");
-        UtlConfigurationFile userFile = UtlConfigurationFile.load(in);
-        RafServiceInstanceConfiguration rafConfigU = (RafServiceInstanceConfiguration) userFile.getServiceInstances().get(1); // RAF
-        RafServiceInstance rafUser = new RafServiceInstance(userFile.getPeerConfiguration(), rafConfigU);
-        rafUser.bind(2);
-
-        Thread.sleep(2000);
-
-        assertEquals(ServiceInstanceBindingStateEnum.READY, rafUser.getCurrentBindingState());
-        assertEquals(ServiceInstanceBindingStateEnum.READY, rafProvider.getCurrentBindingState());
-
-        rafUser.unbind(UnbindReasonEnum.SUSPEND);
-
-        Thread.sleep(2000);
-
-        assertEquals(ServiceInstanceBindingStateEnum.UNBOUND, rafUser.getCurrentBindingState());
-        assertEquals(ServiceInstanceBindingStateEnum.UNBOUND, rafProvider.getCurrentBindingState());
-
-        rafUser.dispose();
-        rafProvider.dispose();
-    }
-
-    @Test
     void testProviderBindUnbind() throws IOException, InterruptedException {
-        // Full logs
-        Logger.getLogger("eu.dariolucia.ccsds").setLevel(Level.ALL);
-        for(Handler h : Logger.getLogger("").getHandlers()) {
-            h.setLevel(Level.ALL);
-        }
         // User
         InputStream in = this.getClass().getClassLoader().getResourceAsStream("configuration_test_user.xml");
         UtlConfigurationFile userFile = UtlConfigurationFile.load(in);
@@ -116,11 +70,6 @@ public class RafTest {
 
     @Test
     void testStatusReportGetParameter() throws IOException, InterruptedException {
-        // Full logs
-        Logger.getLogger("eu.dariolucia.ccsds").setLevel(Level.ALL);
-        for(Handler h : Logger.getLogger("").getHandlers()) {
-            h.setLevel(Level.ALL);
-        }
         // Provider
         InputStream in = this.getClass().getClassLoader().getResourceAsStream("configuration_test_provider.xml");
         UtlConfigurationFile providerFile = UtlConfigurationFile.load(in);
@@ -148,14 +97,82 @@ public class RafTest {
 
         assertEquals(2, recorder.getPduReceived().size());
 
-        // Ask for parameter: expect positive return
+        // Ask for parameter transfer buffer size: expect positive return
+        recorder.getPduReceived().clear();
         rafUser.getParameter(RafParameterEnum.BUFFER_SIZE);
 
         Thread.sleep(2000);
 
-        assertEquals(3, recorder.getPduReceived().size());
-        assertEquals(10, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(2)).getResult().getPositiveResult().getParBufferSize().getParameterValue().intValue());
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(10, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getPositiveResult().getParBufferSize().getParameterValue().intValue());
 
+        // Ask for parameter latency limit: expect positive return
+        recorder.getPduReceived().clear();
+        rafUser.getParameter(RafParameterEnum.LATENCY_LIMIT);
+
+        Thread.sleep(2000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(3, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getPositiveResult().getParLatencyLimit().getParameterValue().getOnline().intValue());
+
+        // Ask for parameter permitted frame quality: expect positive return
+        recorder.getPduReceived().clear();
+        rafUser.getParameter(RafParameterEnum.REQUESTED_FRAME_QUALITY);
+
+        Thread.sleep(2000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(0, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getPositiveResult().getParReqFrameQuality().getParameterValue().intValue());
+
+        // Ask for parameter return timeout: expect positive return
+        recorder.getPduReceived().clear();
+        rafUser.getParameter(RafParameterEnum.RETURN_TIMEOUT_PERIOD);
+
+        Thread.sleep(2000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(120, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getPositiveResult().getParReturnTimeout().getParameterValue().intValue());
+
+        // Ask for parameter min reporting cycle: expect negative return
+        recorder.getPduReceived().clear();
+        rafUser.getParameter(RafParameterEnum.MIN_REPORTING_CYCLE);
+
+        Thread.sleep(2000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(0, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getNegativeResult().getSpecific().intValue());
+
+        // Schedule status report every 5 seconds
+        recorder.getPduReceived().clear();
+        rafUser.scheduleStatusReport(false, 5);
+
+        Thread.sleep(2000);
+
+        assertEquals(2, recorder.getPduReceived().size());
+
+        Thread.sleep(4000);
+
+        assertEquals(3, recorder.getPduReceived().size());
+
+        // Ask for parameter reporting cycle: expect positive return
+        recorder.getPduReceived().clear();
+        rafUser.getParameter(RafParameterEnum.REPORTING_CYCLE);
+
+        Thread.sleep(1000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertEquals(5, ((RafGetParameterReturnV1toV4) recorder.getPduReceived().get(0)).getResult().getPositiveResult().getParReportingCycle().getParameterValue().getPeriodicReportingOn().intValue());
+
+        // Stop status report
+        recorder.getPduReceived().clear();
+        rafUser.scheduleStatusReport(true, null);
+
+        Thread.sleep(2000);
+
+        assertEquals(1, recorder.getPduReceived().size());
+        assertNotNull(((SleScheduleStatusReportReturn) recorder.getPduReceived().get(0)).getResult().getPositiveResult());
+
+        // Unbind
         rafUser.unbind(UnbindReasonEnum.SUSPEND);
 
         Thread.sleep(2000);
