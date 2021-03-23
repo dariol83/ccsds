60d382fa6b135bc2748a9e53fe1baf810e159036
Dario Lucia <dario.lucia@gmail.com>
2021-03-23 18:35:15 +0100

    Added launchers for the UI applications, implemented executable Jar for the Viewer and the SLE UTLFX applications. The Inspector POM file contains instructions to run the application in a full module-compliant fashion (required to load the necessary connectors)

---
+++ eu.dariolucia.ccsds.sle.utlfx/src/main/java/eu/dariolucia/ccsds/sle/utlfx/application/Launcher.java
@@ -0,0 +1,8 @@
+package eu.dariolucia.ccsds.sle.utlfx.application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        SleFxTestTool.main(args);
+    }
+}
---
+++ eu.dariolucia.ccsds.viewer/src/main/java/eu/dariolucia/ccsds/viewer/application/Launcher.java
@@ -0,0 +1,8 @@
+package eu.dariolucia.ccsds.viewer.application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        DataViewer.main(args);
+    }
+}
