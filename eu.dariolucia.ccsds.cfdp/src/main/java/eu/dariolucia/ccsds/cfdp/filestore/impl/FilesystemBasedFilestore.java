1c9e5eec470f0e6eb1924576e6131769e776efd1
Dario Lucia <dario.lucia@gmail.com>
2021-04-21 18:21:43 +0200

    Basic filestore implementation added

---
+++ eu.dariolucia.ccsds.cfdp/src/main/java/eu/dariolucia/ccsds/cfdp/filestore/impl/FilesystemBasedFilestore.java
@@ -0,0 +1,171 @@
+package eu.dariolucia.ccsds.cfdp.filestore.impl;
+
+import eu.dariolucia.ccsds.cfdp.filestore.FilestoreException;
+import eu.dariolucia.ccsds.cfdp.filestore.IVirtualFilestore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A basic filestore, composed of a flat list of directories, each containing a file.
+ * A full path is composed by the format &lt;directory name&gt;/&lt;file name&gt;. &lt;directory name&gt; can be omitted
+ * if the file resides in the root folder of the virtual file store.
+ */
+public class FilesystemBasedFilestore implements IVirtualFilestore {
+
+    public static final String DIR_FILE_SEPARATOR = "/";
+    private final File root;
+
+    public FilesystemBasedFilestore(String absoluteRootPath) {
+        this(new File(absoluteRootPath));
+    }
+
+    public FilesystemBasedFilestore(File root) {
+        if(root == null) {
+            throw new NullPointerException("Provided file is null");
+        }
+        if(!root.exists()) {
+            throw new IllegalArgumentException(String.format("Provided file %s does not exist", root.getAbsolutePath()));
+        }
+        if(!root.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Provided file %s is not a directory", root.getAbsolutePath()));
+        }
+        this.root = root;
+    }
+
+    @Override
+    public void createFile(String fullPath) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(!target.exists()) {
+            try {
+                if(!target.createNewFile()) {
+                    throw new FilestoreException(String.format("Cannot create file %s: createNewFile() returned false", fullPath));
+                }
+            } catch (IOException e) {
+                throw new FilestoreException(String.format("Cannot create file %s: %s", fullPath, e.getMessage()), e);
+            }
+        } else if(target.isDirectory()) {
+            throw new FilestoreException(String.format("Cannot create file %s: directory with the same name exists", fullPath));
+        }
+    }
+
+    @Override
+    public void deleteFile(String fullPath) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(target.exists() && target.isFile()) {
+            try {
+                Files.delete(target.toPath());
+            } catch (IOException e) {
+                throw new FilestoreException(String.format("Cannot delete file %s: %s", fullPath, e.getMessage()), e);
+            }
+        } else if(target.isDirectory()) {
+            throw new FilestoreException(String.format("Cannot delete file %s: the file is a directory", fullPath));
+        }
+    }
+
+    @Override
+    public void renameFile(String fullPath, String newFullPath) throws FilestoreException {
+        File source = constructTarget(fullPath);
+        File target = constructTarget(newFullPath);
+        if(target.exists()) {
+            throw new FilestoreException(String.format("Cannot move file %s to %s: destination file already exists", fullPath, newFullPath));
+        }
+        if(source.exists()) {
+            try {
+                Files.move(source.toPath(), target.toPath());
+            } catch (IOException e) {
+                throw new FilestoreException(String.format("Cannot move file %s to %s: %s", fullPath, newFullPath, e.getMessage()), e);
+            }
+        } else {
+            throw new FilestoreException(String.format("Cannot move file %s to %s: source file does not exist", fullPath, newFullPath));
+        }
+    }
+
+    @Override
+    public void appendFile(String fullPath, byte[] data) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(!target.exists()) {
+            throw new FilestoreException(String.format("Cannot append to file %s: destination file does not exist", fullPath));
+        }
+        try {
+            try (FileOutputStream fos = new FileOutputStream(target, true)) {
+                fos.write(data);
+            }
+        } catch (IOException e) {
+            throw new FilestoreException(String.format("Cannot append to file %s: %s", fullPath, e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public void replaceFile(String fullPath, byte[] data) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(!target.exists()) {
+            throw new FilestoreException(String.format("Cannot replace file %s: destination file does not exist", fullPath));
+        }
+        try {
+            try (FileOutputStream fos = new FileOutputStream(target, false)) {
+                fos.write(data);
+            }
+        } catch (IOException e) {
+            throw new FilestoreException(String.format("Cannot replace file %s: %s", fullPath, e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public void createDirectory(String fullPath) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(!target.exists()) {
+            if(!target.mkdir()) {
+                throw new FilestoreException(String.format("Cannot create directory %s: createDirectory() returned false", fullPath));
+            }
+        } else if(!target.isDirectory()) {
+            throw new FilestoreException(String.format("Cannot create directory %s: file with the same name exists", fullPath));
+        }
+    }
+
+    @Override
+    public void deleteDirectory(String fullPath) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        if(target.exists() && target.isDirectory()) {
+            try {
+                Files.delete(target.toPath());
+            } catch (IOException e) {
+                throw new FilestoreException(String.format("Cannot delete directory %s: %s", fullPath, e.getMessage()), e);
+            }
+        } else if(!target.isDirectory()) {
+            throw new FilestoreException(String.format("Cannot delete directory %s: the path is not a directory", fullPath));
+        }
+    }
+
+    @Override
+    public List<String> listDirectory(String fullPath, boolean recursive) throws FilestoreException {
+        File target = constructTarget(fullPath);
+        try {
+            return Files.list(target.toPath()).map(Path::toFile).map(File::getName).collect(Collectors.toList()); // NOSONAR pointless remark
+        } catch (IOException e) {
+            throw new FilestoreException(String.format("Cannot list directory %s: %s", fullPath, e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public boolean fileExists(String fullPath) {
+        File target = constructTarget(fullPath);
+        return target.exists() && target.isFile();
+    }
+
+    @Override
+    public boolean directoryExists(String fullPath) {
+        File target = constructTarget(fullPath);
+        return target.exists() && target.isDirectory();
+    }
+
+    private File constructTarget(String fullPath) {
+        String[] splt = fullPath.split(DIR_FILE_SEPARATOR, -1);
+        return new File(root.getAbsolutePath() + (splt[0] != null ?  File.separator + splt[0] : "") + File.separator + splt[1]);
+    }
+}
